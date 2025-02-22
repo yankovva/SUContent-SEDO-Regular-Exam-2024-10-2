fab8d7d873c8a19db5c2685af035c968fcd50126
yankovva <maggie.yankovaaa@gmail.com>
2025-02-22 13:32:11 +0100

    Some message

--- Homies.Data/Data/Models/Event.cs
+++ Homies.Data/Data/Models/Event.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static Homies.Data.Data.DataConstants;
