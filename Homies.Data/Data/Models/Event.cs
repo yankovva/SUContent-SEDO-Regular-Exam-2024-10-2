7b0ac5a69b0ecb0aa1668e4a81b83a4109370512
yankovva <maggie.yankovaaa@gmail.com>
2025-02-22 13:33:17 +0100

    Revert "Some message"
    This reverts commit 49864733bdbe47faeaded802ec5abee776b1b08b.

--- Homies.Data/Data/Models/Event.cs
+++ Homies.Data/Data/Models/Event.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static Homies.Data.Data.DataConstants;
